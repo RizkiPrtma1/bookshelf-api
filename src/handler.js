13395668595a93fcbe398a508da72ddf165e4c66
Rizki <rizki.pratama.rp44@gmail.com>
2023-10-02 13:41:34 +0700

    fixes some eslint bugs

--- src/handler.js
+++ src/handler.js
@@ -20,53 +20,75 @@ const addBookHandler = (request, h) => {
     });
     response.code(400);
     return response;
-  } else if (readPage > pageCount) {
+  }
+
+  if (pageCount < readPage) {
     const response = h.response({
       status: "fail",
-      messsage:
+      message:
         "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
     });
     response.code(400);
+
     return response;
-  } else {
-    const id = nanoid(16);
-    const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
-    const finished = pageCount === readPage;
+  }
 
-    const newBook = {
-      id,
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      finished,
-      reading,
-      insertedAt,
-      updatedAt,
-    };
-    books.push(newBook);
-    const isSuccess = books.filter((book) => book.id === id).length > 0;
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
 
-    if (isSuccess) {
-      const response = h.response({
-        status: "success",
-        message: "Buku berhasil ditambahkan",
-        data: {
-          bookId: id,
-        },
-      });
-      response.code(201);
-      return response;
-    }
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+  books.push(newBook);
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Buku berhasil ditambahkan",
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
   }
 };
 
 const getAllBooksHandler = (request, h) => {
-  const filteredBooks = books;
+  const { name, reading, finished } = request.query;
+  let filteredBooks = books;
+
+  if (name !== undefined) {
+    filteredBooks = filteredBooks.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  if (reading !== undefined) {
+    filteredBooks = filteredBooks.filter(
+      (book) => book.reading === !!Number(reading)
+    );
+  }
+
+  if (finished !== undefined) {
+    filteredBooks = filteredBooks.filter(
+      (book) => book.finished === !!Number(finished)
+    );
+  }
   const response = h.response({
     status: "success",
     data: {
